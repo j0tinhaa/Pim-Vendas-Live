fb9682959e6146f1f789ef65e84814236a7db8c2
Pjotah <artist.pjotah@gmail.com>
2026-04-22 15:34:49 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PimVendas.Models;
+
+namespace PimVendas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,65 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+$(function () {
+
+    $('#Emprestimos').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "Nenhum dado disponível na tabela",
+            "info": "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros no total)",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "loadingRecords": "Carregando...",
+            "processing": "Processando...",
+            "search": "Buscar:",
+            "zeroRecords": "Nenhum registro encontrado",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Ordenar por esta coluna",
+                "orderableReverse": "Inverter ordenação"
+            }
+        }
+    });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        })
+    }, 5000)
+
+});
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
+
+// Corrige bug ao redimensionar
+window.addEventListener("resize", function () {
+    const sidebar = document.getElementById("sidebar");
+
+    if (window.innerWidth >= 768) {
+        sidebar.classList.remove("active");
+    }
+});
+
+document.addEventListener("click", function (event) {
+    const sidebar = document.getElementById("sidebar");
+    const button = document.querySelector("button");
+
+    if (
+        window.innerWidth < 768 &&
+        !sidebar.contains(event.target) &&
+        !button.contains(event.target)
+    ) {
+        sidebar.classList.remove("active");
+    }
+});
